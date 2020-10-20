2b2a447fc3f366fa442523418623ce09e69ed9f4
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-10-20 23:02:20 +0300

    Create DLList.h

---
+++ Practicum/week01/DLList.h
@@ -0,0 +1,41 @@
+#pragma once
+
+template<class T>
+struct Node{
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+    Node(const T&, Node<T>* = nullptr, Node<T>* = nullptr);
+};
+
+template<class T>
+class DLList{
+    Node<T>* first;
+    Node<T>* last;
+    size_t currentSize;
+
+    /*---- Helper Functions ----*/
+    void copy(const DLList<T>&);
+    void destroy();
+
+    public:
+        DLList();
+        DLList(const DLList<T>&);
+        DLList<T>& operator=(const DLList<T>&);
+        ~DLList();
+        void push_back(const T& data);
+        void push_front(const T& data);
+        void pop_back();
+        void pop_front();
+        T front() const;
+        T back() const;
+        bool empty() const;
+        size_t size() const;
+        void print() const;
+        void reverse();
+
+        /* ----- Tasks -----*/
+        DLList<T>& mergeWithSymbol(DLList<T>&, DLList<T>&);
+        bool checkLastFirst();
+        void removeClone();
+};
