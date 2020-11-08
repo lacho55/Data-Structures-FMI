de230ba7ac11873b6afa00d26b733c01a350d588
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-11-08 19:19:19 +0200

    Update main.cpp

--- Lections/BinaryTreesTasks/main.cpp
+++ Lections/BinaryTreesTasks/main.cpp
@@ -4,15 +4,20 @@
 int main(){
     BinTree<int> tree;
 
-    tree.insert(2);
+    tree.insert(4);
     tree.insert(3);
 
-   tree.remove(2);
-   tree.remove(3);
+  // tree.remove(4);
+  // tree.remove(3);
 
    // tree.print();
 
+   //TASK 19.1
    tree.count();
+   std:: cout << std::endl;
+
+   //TASK 19.2
+   tree.countEvens();
 
     return 0;
 }
