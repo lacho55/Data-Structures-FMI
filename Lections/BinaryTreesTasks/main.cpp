f6716c6be6da1a450b8ad31ead1ba42205b7cc50
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-11-08 23:21:37 +0200

    Update main.cpp

--- Lections/BinaryTreesTasks/main.cpp
+++ Lections/BinaryTreesTasks/main.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include "binTree.h"
+bool even(const int& v) {
+    return ( v % 2 == 0);
+}
+
 
 int main(){
     BinTree<int> tree;
 
     tree.insert(4);
     tree.insert(3);
+    tree.insert(5);
+    tree.insert(52);
 
   // tree.remove(4);
   // tree.remove(3);
@@ -18,6 +24,9 @@ int main(){
 
    //TASK 19.2
    tree.countEvens();
+
+   std:: cout << tree.searchCount(even);
+    // call_predicate(4.0, foo);
 
     return 0;
 }
